4aad04eb7665cbfea018c2ec865f4cd6f6abfd44
Nils Backe <nils.backe@gmail.com>
2018-06-25 18:32:33 -0400

    Added shared preferences for streak

--- lib/home_state.dart
+++ lib/home_state.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'home_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,9 +14,52 @@ abstract class HomeState extends State<Home> {
   String keepItUp = "Keep It Up!";
   List<bool> isCompletedValues = new List.filled(3, false);
 
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  @override
+  initState() {
+    super.initState();
+    getStreak();
+  }
+
   void alterTask(bool checkboxVal, int index) {
     setState(() {
       isCompletedValues[index] = checkboxVal;
+    });
+    updateStreak();
+  }
+
+  bool allCompleted() {
+    for (bool val in isCompletedValues) {
+      if (!val) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void updateStreak() {
+    if (allCompleted() /* and they weren't last completed today */) {
+      incrementStreak();
+      saveStreak();
+    }
+  }
+
+  void incrementStreak() {
+    setState(() {
+      streak++;
+    });
+  }
+
+  saveStreak() async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setInt('streak', streak);
+  }
+
+  getStreak() async {
+    final SharedPreferences prefs = await _prefs;
+    setState(() {
+      streak = (prefs.getInt('streak') ?? 0);
     });
   }
 }
